5f20cf9c604ce9b8d8ce037151a21cdbf9edfa2e
shenlin <finalair71@gmail.com>
2017-08-20 21:31:18 +0800

    add multiple phone number support

--- sendsms.py
+++ sendsms.py
@@ -5,32 +5,49 @@
 import gammu
 import pyodbc
 import datetime
-smsdata = ""
+
 cnxn = pyodbc.connect(
     'DRIVER=FreeTDS;SERVER=192.168.1.14\SQLHOME;DATABASE=test;UID=sa;PWD=19920530fF;port=49165;')
 
 cursor = cnxn.cursor()
 cursor.execute(
-    "select top 1 id, smsdata from sms where sent='N' order by InsertDate")
+    "select top 1 id, phonenumber, smsdata from sms where sent='N' order by InsertDate")
 row = cursor.fetchone()
-id = row[0]
-smsdata = row[1]
+try:
+    id = row[0]
+    phones = row[1]
+    smsdata = row[2]
+except Exception, e:
+    raise Exception('No SMS Found!')
 # send SMS
 sm = gammu.StateMachine()
 sm.ReadConfig()
 sm.Init()
-
-message = {
-    'Text': smsdata,
-    'SMSC': {'Location': 1},
-    'Number': '+8613032163799',
-    'Coding': 'Unicode_No_Compression',
-}
-
-# Actually send the message
-sm.SendSMS(message)
+if ";" in phones:
+    phoneList = phones.split(';',1)
+    for phone in phoneList:
+        message = {
+        'Text': smsdata,
+        'SMSC': {'Location': 1},
+        'Number': phone.encode('utf-8'),
+        'Coding': 'Unicode_No_Compression',
+        }
+        print message
+        sm.SendSMS(message)
+        print 'message sent'
+else:
+    message = {
+        'Text': smsdata,
+        'SMSC': {'Location': 1},
+        'Number': phones.encode('utf-8'),
+        'Coding': 'Unicode_No_Compression',
+    }
+    print message
+    # Actually send the message
+    sm.SendSMS(message)
+    print 'message sent'
 # update sent sms
 cursor.execute(
-    "UPDATE SMS SET Sent = ?, UpdateDate = ? where id = ?", 'Y', id, datetime.datetime.now())
+    "UPDATE SMS SET Sent = ?, UpdateDate = ? where id = ?", 'Y', datetime.datetime.now(),id)
 cnxn.commit()
 cnxn.close()
